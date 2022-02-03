79dcc5adcecc9d18ba98ed294f3d072aa3098ee5
Park Jin Gon <bennp3038@gmail.com>
2022-02-03 22:29:32 +0900

    init

---
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.*;
+
+public class MemberApp {
+
+
+    /**
+     * 다른 저장소로 변경할때 OCP 원칙을 준수하는가?
+     *
+     *
+     * DIP를 잘 지키고 있나?
+     * MemberSericeImpl의   private static MemberRepository memberRepository = new MemoryMemberRepository(); 부분이
+     * 인터페이스도 의존하고 구현체도 의존하고 있으므로 DIP를 지키지 않고 있음.
+     */
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
